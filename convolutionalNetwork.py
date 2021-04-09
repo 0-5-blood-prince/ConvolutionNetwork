4670dfecabfa383da8b148b133e64d19c2447234
Jaitesh Pachipulusu <jaiteshp@gmail.com>
2021-04-09 20:08:58 +0530

    basic setup

---
+++ convolutionalNetwork.py
@@ -0,0 +1,21 @@
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+
+class ConvNet:
+    def __init__(self, numFilters, filterSize, filterConf = 'same'):
+
+    def initModel(self, numFilters, filterSize, filterConf = 'same'):
+        model = Sequential()
+        model.add(Conv2D(numFilters, (filterSize, filterSize), input_shape=input_shape))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))        
+        
+        for(l in range(4)):
+            model.add(Conv2D(numFilters, (filterSize, filterSize)))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+        
+        model.add(FLatten())
+        #model.add(Dense(128, activation='relu'))
+        model.add(Dense(10, activation='softmax'))
+        
